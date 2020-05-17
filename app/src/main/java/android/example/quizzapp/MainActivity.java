38be18d2c18394d06cb06a07e68b391a3da4c842
Geoffrey Bosire Jr <gbosirejr@gmail.com>
2020-05-16 17:02:08 -0800

    created a new xml layout, new string values, and added id values to answers

--- app/src/main/java/android/example/quizzapp/MainActivity.java
+++ app/src/main/java/android/example/quizzapp/MainActivity.java
@@ -18,44 +18,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method is called when the submit button is clicked.
-     */
-    public void answer(View view) {
-        CheckBox answerTwoQuestionOne = findViewById(R.id.q1A2);
-        boolean correctAnswerQuestionOne = answerTwoQuestionOne.isChecked();
-        questionOneAnswer(correctAnswerQuestionOne);
-
-
-
-    }
-
-    /**
      * This method makes a toast for question one and global variable @correctScore keeps correct score.
      */
-    public void questionOneAnswer(boolean correctAnswer){
-        CheckBox answerOneQuestionOne = findViewById(R.id.q1A1);
-        boolean questionOneWrongAnswerOne = answerOneQuestionOne.isChecked();
-        CheckBox answerThreeQuestionOne = findViewById(R.id.q1A3);
-        boolean questionOneWrongAnswerThree = answerThreeQuestionOne.isChecked();
-        CheckBox answerFourQuestionOne = findViewById(R.id.q1A4);
-        boolean questionOneWrongAnswerFour = answerFourQuestionOne.isChecked();
 
-        if (correctAnswer == true){
-        Toast.makeText(this, "Correct Answer", Toast.LENGTH_SHORT).show();
-        correctScore +=1;
-        displayScore(correctScore);
-        }
+    public void displayTotal(View view) {
 
-        if (questionOneWrongAnswerOne || questionOneWrongAnswerThree || questionOneWrongAnswerFour == true){
-            Toast.makeText(this, "Wrong Answer", Toast.LENGTH_SHORT).show();
-        }
-
-    }
-
-
-    private void displayScore(int Score){
-        TextView scoreTotal = findViewById(R.id.score);
-        scoreTotal.setText(Score);
     }
 }
+
 
