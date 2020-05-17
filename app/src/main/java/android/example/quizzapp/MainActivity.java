b9f0d53381aa4384940f3721cce0bbaceb1be06e
Geoffrey Bosire Jr <gbosirejr@gmail.com>
2020-05-17 14:06:00 -0800

    updated display total method to calculate correct score and display toast message | App Complete

--- app/src/main/java/android/example/quizzapp/MainActivity.java
+++ app/src/main/java/android/example/quizzapp/MainActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,10 +20,40 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * This method makes a toast for question one and global variable @correctScore keeps correct score.
+     *
      */
 
     public void displayTotal(View view) {
+        RadioButton correctAnswerQuestion1 = findViewById(R.id.q1C);
+        RadioButton correctAnswerQuestion2 = findViewById(R.id.q2D);
+        CheckBox question3CorrectAnswer1 = findViewById(R.id. q3B);
+        CheckBox question3CorrectAnswer2 = findViewById(R.id. q3C);
+        CheckBox question3CorrectAnswer3 = findViewById(R.id. q3D);
+        EditText question4Response = findViewById(R.id.yesNoResponse);
+        String question4String = question4Response.getText().toString().toLowerCase();
 
+        if (correctAnswerQuestion1.isChecked()){
+            correctScore +=1;
+        }
+
+        if (correctAnswerQuestion2.isChecked()){
+            correctScore +=1;
+        }
+
+        if (question3CorrectAnswer1.isChecked() && question3CorrectAnswer2.isChecked() && question3CorrectAnswer3.isChecked()) {
+            correctScore +=1;
+        }
+
+        if (question4String.equals("yes")){
+            correctScore +=1;
+        }
+
+        if (question4String.equals("no")){
+            correctScore +=1;
+        }
+
+        Toast.makeText(this, "Total Score is " + correctScore + " out of ", Toast.LENGTH_SHORT).show();
+        correctScore = 0;
     }
 }
 
